fb93169e2b8a6ea8f1cca4c008ede9465fbc7f93
Daniel Okechukwu <dikedaniel7917@gmail.com>
2022-09-01 14:27:36 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import List from './components/Lists';
+import AddToList from './components/AddToLists';
+
+export interface IState {
+  people: {
+      name: string
+      age: number
+      img: string
+      note?: string
+  }[]
+}
+
 
 function App() {
+
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "LeBron James",
+      age: 36,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbNYTeJMPyneKPa7vsAQuqw_A4YLQHaT1wWQ&usqp=CAU",
+      note: "G.O.A.T 🐐🐐🐐🐐🐐🐐",
+    },
+    {
+      name: "Kobe Bryant",
+      age: 42,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHeQjgbZrnBCgy1JnTAd6kAs-D6vv8MWV7A&usqp=CAU",
+      note: "Mamba Forever"
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List people={people}/>
+      <AddToList setPeople={setPeople} people={people}/>
     </div>
   );
 }
